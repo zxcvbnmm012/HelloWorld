0c752ddf717b28b633a58217a1888300267241c3
admin <khmin3305@naver.com>
2025-05-14 10:47:12 +0900

    첫번째 릴리즈

---
+++ HelloWorld/src/com/yedam/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.yedam;
+
+// 자바 : 컴파일(***.java -> ***.class)
+
+public class HelloWorld {
+	// main method
+   public static void main(String[] args) { // 문자열 배열인 args 변수를 매개값으로 받음
+	   for (int i = 1; i <= 3; i++) {
+		   System.out.println("Hello World"); // 출력 명령어 : System.out.prinln
+	   }
+   }
+}
