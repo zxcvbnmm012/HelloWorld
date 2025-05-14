a40295271b2ce432a7bdcb51d98d6648057c5eff
admin <khmin3305@naver.com>
2025-05-14 12:04:05 +0900

    복습

--- HelloWorld/src/com/yedam/HelloWorld.java
+++ HelloWorld/src/com/yedam/HelloWorld.java
@@ -2,7 +2,8 @@ package com.yedam;
 
 // 자바 : 컴파일(***.java -> ***.class)
 
-public class HelloWorld {
+public class HelloWorld { // class 명과 파일 이름 동일
+	// 대문자로 시작하면 class
 	// main method
    public static void main(String[] args) { // 문자열 배열인 args 변수를 매개값으로 받음
 	   for (int i = 1; i <= 3; i++) {
---
+++ HelloWorld/src/com/yedam/JSexe.java
@@ -0,0 +1,27 @@
+package com.yedam;
+
+public class JSexe {
+	public static void main(String[] args) {
+		// 임의의 수를 생성 : 1~10 사이의 임의의 값 생성
+		int sum = 0;
+		for(int i = 1 ; i <= 5 ; i++) {
+			int result = (int)(Math.random() * 10) + 1; // 1 <= x < 11
+			// Math.random() : 0 <= x < 1 범위의 실수를 생성하는 메소드
+			sum += result;
+		}
+		System.out.println("결과: " + sum);
+		
+	} // end of main()
+	
+	public static void test() {    // main 에서 test 함수를 호출해서 실행
+		int sum = 0;
+		// 1 ~ 10 까지의 누적 반복문
+		for (int i = 1 ; i <= 10 ; i++) {
+			if (i % 2 == 1) { // 홀수 : 1, 3, 5, 7, 9
+				sum += i;	
+			}
+		} 
+		System.out.println("결과: " + sum);
+		
+	} // end of test()
+} // end of class
